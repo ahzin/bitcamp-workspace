b1aaed2ace1adb740f8431052296fd14b22ebf59
AHZIN <ahzin0605@gmail.com>
2020-08-05 13:24:13 +0900

    --

--- bitcamp-java-basic/src/main/java/com/eomcs/oop/ex02/Exam0210.java
+++ bitcamp-java-basic/src/main/java/com/eomcs/oop/ex02/Exam0210.java
@@ -5,16 +5,39 @@ import com.eomcs.util.Calculator;
 public class Exam0210 {
 
   public static void main(String[] args) {
-    // 2 + 3 - 1 * 7 / 3 = ? 을 계산 (단, 연산자 우선순위가 없다는 가정)
-    int result = 0;
-    Calculator.plus(2);
-    Calculator.plus(3);
-    Calculator.minus(1);
-    Calculator.multiple(7);
-    Calculator.devide(3);
+    // 식 1)  2 + 3 - 1 * 7 / 3 = ? 을 계산 (단, 연산자 우선순위가 없다는 가정)
+    // 식 2)  3 * 2 + 7 /4 -5 =?
 
-    printResult(Calculator.result);
+    //식 1과 2를 동시에 계산할 수 있을까?
+    // => 불가하다 !
+    // => Calculator는 result 변수를 한 개만 관리하기 때문에
+    //      동시에 여러 개의 식 결과를 따로 관리할 수 없다.
+    //식 1과 2를 동시에 계산하려면 result변수를 2개 준비해야한다.
 
+    Calculator c1 = new Calculator();
+    Calculator c2 = new Calculator();
+
+
+    //식 1
+    c1.plus(2);
+    c1.plus(3);
+    c1.minus(1);
+    c1.multiple(7);
+    c1.devide(3);
+
+    printResult(c1.result);
+
+    //식 2
+    c2.plus(3);
+    c2.multiple(2);
+    c2.plus(7);
+    c2.devide(4);
+    c2.minus(5);
+
+    printResult(c2.result);
+
+    //result변수는 1개이기 때문에 동시에 계산하지 못한다.
+    //계산이 끝난 후, 다시 result변수를 0으로 초기화해서 사용해야한다.
   }
 
   public static void printResult(int value) {
--- bitcamp-java-basic/src/main/java/com/eomcs/util/Calculator.java
+++ bitcamp-java-basic/src/main/java/com/eomcs/util/Calculator.java
@@ -2,24 +2,36 @@ package com.eomcs.util;
 
 public class Calculator{
 
-  //클래스를 Method Area에 로딩할 때, static 영역에 변수를 따로 생성한다.
+  //클래스를 Method Area에 로딩할 때, static 영역에 따로 변수를 생성한다.
   // => Heap에 생성하는 것이 아니다.
-  public static int result = 0;
 
-  public static void plus(int value) {
-    result += value;
+  //result변수는 각 계산마다 개별적으로 관리해야한다.
+  // non-static변수로 선언해야한다.
+  // non-static변수는 new 연산자를 통해서만 만들 수 있다.
+  // non-static변수는 heap 영역에 생성된다.
+
+  public int result = 0;
+
+  // static 매서드는 non-static 변수를 사용할 수 없다.
+  // 사용하려면 new 명령을 통해 생성된 메모리에 주소를 알아야한다.
+
+
+  //non-static 메서드(인스턴스 메서드)는 인스턴스 주소를 받는
+  //this라는 빌트인 변수가 있다.
+  public void plus(int value) {
+    this.result += value;
   }
 
-  public static void minus(int value) {
-    result -= value;
+  public  void minus(int value) {
+    this.result -= value;
   }
 
-  public static void multiple(int value) {
-    result *= value;
+  public void multiple(int value) {
+    this.result *= value;
   }
 
-  public static void devide(int value) {
-    result /= value;
+  public void devide(int value) {
+    this.result /= value;
   }
 
 }
