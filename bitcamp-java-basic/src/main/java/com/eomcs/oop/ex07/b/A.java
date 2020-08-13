6d3ceaa552e87ee77d1ef89f76d586cd03d9f793
AHZIN <ahzin0605@gmail.com>
2020-08-13 09:38:16 +0900

    corelib

--- bitcamp-java-basic/src/main/java/com/eomcs/oop/ex07/b/A.java
+++ bitcamp-java-basic/src/main/java/com/eomcs/oop/ex07/b/A.java
@@ -1,5 +1,5 @@
 package com.eomcs.oop.ex07.b;
-
+//같은 패키지에 있음
 public class A {
   private int privateVar;
   int defaultVar;
--- bitcamp-java-basic/src/main/java/com/eomcs/oop/ex07/b/sub/B.java
+++ bitcamp-java-basic/src/main/java/com/eomcs/oop/ex07/b/sub/B.java
@@ -1,5 +1,5 @@
 package com.eomcs.oop.ex07.b.sub;
-
+//다른 패키지(sub)
 public class B {
   private int privateVar;
   int defaultVar;
