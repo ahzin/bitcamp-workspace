4ea894506708a87bfd6b263b01016fc1c74cfbdd
AHZIN <ahzin0605@gmail.com>
2020-08-18 15:01:08 +0900

    -

--- bitcamp-java-basic/src/main/java/com/eomcs/algorithm/data_structure/stack/MyStack.java
+++ bitcamp-java-basic/src/main/java/com/eomcs/algorithm/data_structure/stack/MyStack.java
@@ -3,11 +3,7 @@ package com.eomcs.algorithm.data_structure.stack;
 import java.util.EmptyStackException;
 import com.eomcs.algorithm.data_structure.linkedlist.MyLinkedList;
 
-//1) Stack을 구현하기 위해 기존에 작성한 MyLinkedList를 상속받는다.
-//2) Stack에 값을 추가하는 push() 메서드를 정의한다.
-//3) Stack에서 제일 마지막에 추가한 값을 꺼내는 pop()을 정의한다.
-//4) Stack에서 제일 마지막에 입력한 값을 조회하는 peek()을 정의한다.
-public class MyStack extends MyLinkedList {
+public class MyStack extends MyLinkedList{
 
   public Object push(Object item) {
     add(item);
@@ -15,17 +11,18 @@ public class MyStack extends MyLinkedList {
   }
 
   public Object pop() {
-    if (size() == 0) {
+    if(size() == 0) {
       throw new EmptyStackException();
     }
-    return remove(size()-1);
+    return remove(size() -1);
   }
 
   public Object peek() {
-    if (size() == 0) {
+    if(size() == 0) {
+      System.out.println("null");
       throw new EmptyStackException();
     }
-    return get(size()-1);
+    return get(size() -1);
   }
 
 }
--- bitcamp-java-basic/src/main/java/com/eomcs/algorithm/data_structure/stack/MyStackTest.java
+++ bitcamp-java-basic/src/main/java/com/eomcs/algorithm/data_structure/stack/MyStackTest.java
@@ -3,28 +3,29 @@ package com.eomcs.algorithm.data_structure.stack;
 public class MyStackTest {
   public static void main(String[] args) {
     MyStack stack = new MyStack();
-    stack.push("aaa");
-    stack.push("bbb");
-    stack.push("ccc");
-    System.out.println("==> " + stack.peek()); //ccc
-    stack.push("ddd");
-    stack.push("eee");
-
-    print(stack); //aaa,bbb,ccc,ddd,eee,
+    stack.add("aaa");
+    stack.add("bbb");
+    System.out.println("-->" + stack.peek()); //bbb
+    stack.add("ccc");
+    stack.add("ddd");
+    stack.add("eee");
+    print(stack);
 
     System.out.println(stack.pop()); //eee
     System.out.println(stack.pop()); //ddd
-    System.out.println("==> " + stack.peek()); //ccc
+    System.out.println("-->" + stack.peek()); //ccc
     System.out.println(stack.pop()); //ccc
     System.out.println(stack.pop()); //bbb
     System.out.println(stack.pop()); //aaa
-    System.out.println(stack.pop()); //예외 발생
+    System.out.println("-->" + stack.peek()); //null
+    //Exception in thread "main" java.util.EmptyStackException
+    System.out.println(stack.pop()); //Exception in thread "main" java.util.EmptyStackException
 
   }
 
   static void print(MyStack stack) {
     for(int i = 0; i < stack.size(); i++) {
-      System.out.print(stack.get(i)+",");
+      System.out.print(stack.get(i) + ",");
     }
     System.out.println();
   }
